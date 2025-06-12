456f7881d6ec40a1275d236ed100415137262859
antony Auvray <antony.auvray@hotmail.com>
2025-06-12 17:29:54 +0200

    modif fichier json

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,13 @@ export default {
   theme: {
     extend: {
       colors: {
+        // 07AAB0
         customBlue: "rgb(55,113,126)",
+                // #60A3A3
         customBlue2: "rgb(97,162,163)",
+// BADBDA
         customBlue3: "rgb(186,219,218)",
+
       },
     },
     fontFamily: {
