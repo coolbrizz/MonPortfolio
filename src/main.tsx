00664818ed5f2d7064ff37666bc5b030e4a8634b
antony Auvray <antony.auvray@hotmail.com>
2025-06-12 17:57:14 +0200

    knverq

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,15 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import App from './App.tsx';
-import MentionsLegales from './MentionsLegales.tsx';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from "./App.tsx";
+import MentionsLegales from "./MentionsLegales.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router basename="/MonPortfolio">
+    <Router>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/MonPortfolio" element={<App />} />
         <Route path="/mentions-legales" element={<MentionsLegales />} />
       </Routes>
     </Router>
